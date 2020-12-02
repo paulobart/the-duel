79fbc7842cd67b6fbed37d725e992d738a88ccee
paulobart <paulobart@DESKTOP-8803LBQ.localdomain>
2020-12-02 01:41:32 +0000

    logica de tiro Ok. Colocar restart para 2 e 3 rounds

--- src/script.js
+++ src/script.js
@@ -1,23 +1,18 @@
 
     //startGame()
 
-    let controller = false
+    let controllerLeftPlayer = false
     document.addEventListener("keydown", (e) => {
-        //left player
-      if(e.keyCode === 65 && !controller) {
-        controller = true;
-        console.log(e.keyCode)
+      if(e.keyCode === 65 && !controllerLeftPlayer) {
+        controllerLeftPlayer = true;
         rightPlayer.lives -= 1       
-        // chamar a funcao do tiro
-      }
-        // right player
-      if (e.keyCode === 76 && !controller) {
-        controller = true
-        leftPlayer.lives -= 1
-      }
-    })
-    
+    }
+  });
 
-    
-
-
+  let controllerRightPlayer = false
+    document.addEventListener("keydown", (e) => {
+      if(e.keyCode === 65 && !controllerRightPlayer) {
+        controllerRightPlayer = true;
+        leftPlayer.lives -= 1       
+    }
+  });
